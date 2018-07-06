134a613c0df643cec0a8bf7e1a714cdaa2e95583
ultimatixarup <arupkumar.banerjee@gmail.com>
2018-07-06 11:53:15 -0500

    update

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express  = require('express');
 var app      = express();
-var morgan = require('morgan');
+
 var bodyParser = require('body-parser');
 var cors = require('cors');
 
-app.use(morgan('dev'));
+
 app.use(bodyParser.urlencoded({'extended':'true'}));
 app.use(bodyParser.json());
 app.use(cors());
